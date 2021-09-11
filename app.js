089cb460a673905a17c6e86e28af2f84d21629d0
Setanjan Roy <setanjan@unifiedinfotech.net>
2021-09-11 15:41:11 +0530

    1. Randomize Meme Search 2. Caching

--- app.js
+++ app.js
@@ -7,20 +7,49 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+const NodeCache = require( "node-cache" );
+global.nodeCache = new NodeCache();
+
 
 app.post('/api/search',async (req,res)=>{
         const searchParam = req.body.search;
-        console.log(searchParam)
+        // console.log(searchParam)
         const finalURL = BASE_URL+"search?q="+searchParam;
+        let imgArray = []
         try {
-            let response = await axios.get(finalURL);
-            let $ = cheerio.load(response.data);
-            const grid = $('.entry-grid-body');
-            const searchItem = grid.find('tr td a')[0];
-            let x = BASE_URL+searchItem.attribs.href+'/photos';
-            response = await axios.get(x);
-            $ = cheerio.load(response.data);
-            const img = $('meta[property="og:image"]').attr('content')
+            if(global.nodeCache.has(searchParam)) {
+              imgArray = global.nodeCache.get(searchParam)  
+            } else {
+              let response = await axios.get(finalURL);
+              let $ = cheerio.load(response.data);
+              const grid = $('.entry-grid-body');
+              const searchItem = grid.find('tr td a')[0];
+              let photos = BASE_URL+searchItem.attribs.href+'/photos';
+              response = await axios.get(photos);
+              $ = cheerio.load(response.data);
+              const imgTop = $('meta[property="og:image"]').attr('content')
+              const imgGallery =  $("a[rel=photo_gallery]")
+              for(let i=0;i<imgGallery.length;i++){
+                imgArray.push({url:imgGallery[i].attribs.href,top:false})
+              }
+              imgArray.push({url:imgTop,top:true})
+              global.nodeCache.set(searchParam,imgArray,86400)
+            }
+            const index = Math.floor(Math.random()*imgArray.length)
+            const randomUrl = BASE_URL+imgArray[index].url
+            let img
+            if(imgArray[index].top === true ) {
+                img = randomUrl
+            } else {
+                if(global.nodeCache.has(randomUrl)) {
+                  img = global.nodeCache.get(randomUrl)
+              } else {
+                response = await axios.get(randomUrl);
+                $ = cheerio.load(response.data);
+                img = $('meta[property="og:image"]').attr('content')
+                global.nodeCache.set(randomUrl,img,8640)
+                }
+            }
             return res.status(200).json({
                 "messages": [
                   {
