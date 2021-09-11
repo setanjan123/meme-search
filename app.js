87de57d6ad232ec0a8ac264c863cf6284da3594f
Setanjan Roy <setanjan@unifiedinfotech.net>
2021-09-11 10:57:58 +0530

    handling errors adding package.json

--- app.js
+++ app.js
@@ -5,21 +5,30 @@ const cheerio = require('cheerio')
 const BASE_URL="http://knowyourmeme.com/";
 
 
-app.get('/:search',async (req,res)=>{
+app.get('/api/:search',async (req,res)=>{
         const searchParam = req.params.search;
         const finalURL = BASE_URL+"search?q="+searchParam;
-        let response = await axios.get(finalURL);
-        let $ = cheerio.load(response.data);
-        const grid = $('.entry-grid-body');
-        const searchItem = grid.find('tr td a')[0];
-        let x = BASE_URL+searchItem.attribs.href+'/photos';
-        response = await axios.get(x);
-        $ = cheerio.load(response.data);
-        const img = $('meta[property="og:image"]').attr('content')
-        return res.status(200).json({
-               status:200,
-               data:img
-        })
+        try {
+            let response = await axios.get(finalURL);
+            let $ = cheerio.load(response.data);
+            const grid = $('.entry-grid-body');
+            const searchItem = grid.find('tr td a')[0];
+            let x = BASE_URL+searchItem.attribs.href+'/photos';
+            response = await axios.get(x);
+            $ = cheerio.load(response.data);
+            const img = $('meta[property="og:image"]').attr('content')
+            return res.status(200).json({
+                   status:200,
+                   data:img
+            })
+        } catch(e) {
+              console.log('Error '+e.toString())
+              return res.status(500).json({
+                status:500,
+                message:'Something went wrong'
+         })
+    }
+       
 })
 
 app.listen(3000,()=>{
